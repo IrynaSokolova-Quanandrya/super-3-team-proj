68f585de4a60094ede3986180037fba649b027d6
IrynaSokolova-Quanandrya <80633345+IrynaSokolova-Quanandrya@users.noreply.github.com>
2023-01-01 12:35:51 +0000

    Deploying to gh-pages from @ IrynaSokolova-Quanandrya/super-3-team-proj@e5aa6cb508e31d26c4369b696bea8ea4e65e55af 🚀

---
+++ index.76ea9769.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".header-hamburger"),t=document.querySelector(".header");e.addEventListener("click",(function(){t.classList.toggle("active")}));
+//# sourceMappingURL=index.76ea9769.js.map
---
+++ index.81a85056.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".header-hamburger"),t=document.querySelector(".header");e.addEventListener("click",(function(){t.classList.toggle("active")}))}();
+//# sourceMappingURL=index.81a85056.js.map
